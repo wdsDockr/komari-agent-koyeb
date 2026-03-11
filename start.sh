2915c8769a52c61c602aacb0f672df92a3725494
wdsDockr <wodes@wodes.serv00.net>
2026-03-12 00:10:05 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,2 @@
 #!/bin/sh
 /app/komari-agent --help
-mkdir -p /tmp/www
-echo "OK" > /tmp/www/index.html
-
-while true; do
-  echo -e "HTTP/1.1 200 OK\r\nContent-Length: 2\r\n\r\nOK" | nc -l -p 80 -q 1 2>/dev/null || \
-  echo -e "HTTP/1.1 200 OK\r\nContent-Length: 2\r\n\r\nOK" | nc -l 80 2>/dev/null
-done &
-
-exec /app/komari-agent
