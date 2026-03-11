fdcb4acdbf8dd1a68b377394304e20bed74442a8
wdsDockr <wodes@wodes.serv00.net>
2026-03-11 23:55:58 +0800

    fix: /app/komari-agent

--- start.sh
+++ start.sh
@@ -7,8 +7,4 @@ while true; do
   echo -e "HTTP/1.1 200 OK\r\nContent-Length: 2\r\n\r\nOK" | nc -l 80 2>/dev/null
 done &
 
-# 找出 komari-agent 在哪里
-find / -name "komari-agent" 2>/dev/null
-echo "PATH is: $PATH"
-
-exec komari-agent
+exec /app/komari-agent
